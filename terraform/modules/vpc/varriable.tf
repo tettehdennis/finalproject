987f31aa28b91e213d5fa060d10dfaa38ff6c318
tettehdennis <tettehdennis18@gmail.com>
2024-02-12 22:21:39 +0000

    vpc modules to be continued

---
+++ terraform/modules/vpc/varriable.tf
@@ -0,0 +1,11 @@
+# environment variables
+variable "region" {}
+variable "project_name" {}
+variable "environment" {}
+
+# vpc variables
+variable "vpc_cidr" {}
+variable "public_subnet_az1_cidr" {}
+variable "public_subnet_az2_cidr" {}
+variable "private_subnet_az1_cidr" {}
+variable "private_subnet_az2_cidr" {}
