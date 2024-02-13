017b7296cb00359fbc1b6393a02d936e8b411f78
tettehdennis <tettehdennis18@gmail.com>
2024-02-13 04:00:36 +0000

    created nat gatway module

---
+++ terraform/modules/nat-gatway/variable.tf
@@ -0,0 +1,8 @@
+# nat gatway variables
+variable "project_name" {}
+variable "environment" {}
+variable "public_subnet_az1_id" {}
+variable "var.internet_gateway" {}
+variable "var.vpc.id" {}
+variable "private_subnet_az1_id" {}
+variable "private_subnet_az2_id" {}
